c81b2ec82f30350a27e27f61127d892e2b49bf51
munsu <anstnsp@finger.co.kr>
2019-01-11 10:15:36 +0900

    설정파일반영

--- PromiseTest.1.js
+++ PromiseTest.1.js
@@ -20,12 +20,13 @@ const request = require("request");
 //         resolve(html);
 //     });
 // });
-
+   //한번에 모든 프라미슈를 병렬로 처리하고 결과를 resolve에 반환 
 // Promise.all([naver,daum,google]).then((resolve) =>{
 //     console.log(resolve);
 // })
 
 //Promise chaining
+//프로미슈 결과를 리턴하고 그결과를 다음 then 의 파라미터로 이용가능.
 let p2 = new Promise((resolve,reject)=> {
     resolve(1);
 });
